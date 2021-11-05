d205fe84eb379dec2a7a51f94807b0ebc6b723c1
Hansss <79178335+5Hyeons@users.noreply.github.com>
2021-11-06 00:22:20 +0900

    feat: cosine restart

---
+++ configs/_base_/schedules/schedule_cr_40k.py
@@ -0,0 +1,18 @@
+# optimizer
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer_config = dict()
+# learning policy
+lr_config = dict(
+    _delete_=True,
+    policy='CosineRestart',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    min_lr=0.0,
+    periods=[8000,8000,8000,8000,8000],
+    restart_weights=[1,0.5,0.25,0.125,0.0625],
+    by_epoch=False)
+# runtime settings
+runner = dict(type='IterBasedRunner', max_iters=40000)
+checkpoint_config = dict(by_epoch=False, interval=4000)
+evaluation = dict(interval=4000, metric='mIoU')
